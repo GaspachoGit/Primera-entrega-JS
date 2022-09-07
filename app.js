ab14ae8e0430ccce7da4babaad6b065ed7c62c8d
GaspachoWeb <gaston2.0409@gmail.com>
2022-09-07 13:16:54 -0300

    "Añadido manteles"

--- app.js
+++ app.js
@@ -17,17 +17,14 @@ Posibilidades:
 
 const carrito = []
 
-/* function añadirAlCarrito(stock, precio, carritoCant, carritoPrecio) {
-    if (stock >= 1) {
-        carritoCant = carritoCant + 1;
-        stock = stock - 1
-        carritoPrecio = carritoPrecio + precio;
-        alert("producto añadido al carrito exitosamente");
-    } else {
-        alert("este producto no posee stock");
+class Carrito{
+    constructor(nombre, precio, stock){
+        this.nombre = nombre
+        this.precio = precio
+        this.stock = stock
     }
 }
-*/
+
 function añadirAlCarrito(cortinas) {
     let preguntaStock = Number(prompt("¿Qué cantidad de cortinas a comprar?"));
     let stock = cortinas.stock - preguntaStock;
@@ -43,13 +40,6 @@ function añadirAlCarrito(cortinas) {
     }
 }
 
-class Carrito{
-    constructor(nombre, precio, stock){
-        this.nombre = nombre
-        this.precio = precio
-        this.stock = stock
-    }
-}
 
 function Producto(nombre, precio, stock, color, dimenciones) {
     this.nombre = nombre;
@@ -61,7 +51,10 @@ function Producto(nombre, precio, stock, color, dimenciones) {
 
 const cortina1 = new Producto("Cortina 1", 5500, 5, "roja", "2x4 metros");
 const cortina2 = new Producto("Cortina 2", 2750, 0, "morada", "2x1.5 metros");
-const cortina3 = new Producto("Cortina 3", 1850, 6, "roja", "1x1 metro");
+const cortina3 = new Producto("Cortina 3", 1850, 6, "celeste", "1.5x1.5 metros");
+const mantel1 = new Producto("Mantel 1", 2350, 6, "blanco", "2.3x1.5 metros");
+const mantel2 = new Producto("Mantel 2", 2850, 3, "rojo y blanco", "2.3x1.5 metros");
+const mantel3 = new Producto("Mantel 3", 2350, 6, "amarillo", "2.3x1.5 metros");
 
 let comprar = confirm("¿Desea comprar?");
 
@@ -69,16 +62,28 @@ while (comprar){
     var respuesta = prompt("Ingrese una opcion, (producto stock) para checkear el stock de un producto, (producto) para obtener la informacion del mismo, (producto add) para añadir al carrito (carrito) para ver la información del carrito");
     switch (respuesta) {
 //verificación de stock
-                case ("cortina1 stock"):
+                case "cortina1 stock":
                     alert(cortina1.stock)
                     break;
 
-                case ("cortina2 stock"):
+                case "cortina2 stock":
                     alert(cortina2.stock)
                     break;
 
                 case ("cortina3 stock"):
                     alert(cortina3.stock)
+                    break;
+
+                case ("mantel1 stock"):
+                    alert(mantel1.stock)
+                    break;
+
+                case ("mantel2 stock"):
+                    alert(mantel2.stock)
+                    break;
+
+                case ("mantel3 stock"):
+                    alert(mantel3.stock)
                     break;
 //vista general del priducto
                 case ("cortina1"):
@@ -92,6 +97,18 @@ while (comprar){
                 case ("cortina3"):
                     console.log(cortina3)
                     break;
+
+                case ("mantel1"):
+                    console.log(mantel1)
+                    break;
+
+                case ("mantel2"):
+                    console.log(mantel2)
+                    break;
+
+                case ("mantel3"):
+                    console.log(mantel3)
+                    break;
 //añadir un rpoducto al carrito
                 case ("cortina1 add"):
                     añadirAlCarrito(cortina1)
@@ -103,6 +120,18 @@ while (comprar){
 
                 case ("cortina3 add"):
                     añadirAlCarrito(cortina3)
+                    break;
+
+                case ("mantel1 add"):
+                    añadirAlCarrito(mantel1)
+                    break;
+
+                case ("mantel2 add"):
+                    añadirAlCarrito(mantel2)
+                    break;
+
+                case ("mantel3 add"):
+                    añadirAlCarrito(mantel3)
                     break;
 // check del carrito
                 case ("carrito"):
